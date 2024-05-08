a9bc66296f01167c528b1e201d161dce7efaed27
DarthSharkie <darthsharkie@gmail.com>
2024-05-07 22:04:51 -0700

    Ore: Bauxite
    Spawns Bauxite Ore with parameters roughly in-between those of
    iron and copper.  This does not copy the small, large, lower,
    middle, upper, or any other 'duplicate' features.

--- src/main/java/me/sharkie/minecraft/sharkieblocktest/SharkieBlockTest.java
+++ src/main/java/me/sharkie/minecraft/sharkieblocktest/SharkieBlockTest.java
@@ -2,6 +2,8 @@ package me.sharkie.minecraft.sharkieblocktest;
 
 import me.sharkie.minecraft.sharkieblocktest.toolmaterial.AluminumToolMaterial;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -13,13 +15,17 @@ import net.minecraft.item.Items;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 
 public class SharkieBlockTest implements ModInitializer {
+    public static final String MODID = "sharkie-blocktest";
 
     public static final Block BAUXITE_BLOCK = new Block(FabricBlockSettings.create().strength(4.0f, 4.0f).requiresTool().mapColor(MapColor.DULL_RED));
     public static final Item BAUXITE_ITEM = new BlockItem(BAUXITE_BLOCK, new Item.Settings());
-    public static final String MODID = "sharkie-blocktest";
     public static final Identifier BAUXITE_ORE_IDENTIFIER = new Identifier(MODID, "bauxite_ore");
 
     public static final Item ALUMINUM_INGOT = new Item(new Item.Settings());
@@ -39,5 +45,8 @@ public class SharkieBlockTest implements ModInitializer {
 
         Registry.register(Registries.ITEM, ALUMINUM_PICKAXE_IDENTIFIER, ALUMINUM_PICKAXE);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> content.addAfter(Items.IRON_PICKAXE, ALUMINUM_PICKAXE));
+
+        RegistryKey<PlacedFeature> bauxite_ore_placed_key = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MODID, "ore_bauxite"));
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, bauxite_ore_placed_key);
     }
 }
